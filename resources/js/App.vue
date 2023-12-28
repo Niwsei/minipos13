58713c84019ef4f6125faaedf2d8fc618866c227
Niwsei <loveonelove2004@gmail.com>
2023-12-28 19:58:28 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+<h1>Vue App</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Minipos13App',
+
+
+        adta(){
+            return {
+
+            };
+        },
+        mounted(){
+
+        },
+        methods: {
+
+        },
+
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
